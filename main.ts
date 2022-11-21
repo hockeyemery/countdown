aabf19c700f34f58896e665b202e0e59a0493607
hockeyemery <113910751+hockeyemery@users.noreply.github.com>
2022-11-21 09:08:26 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,14 @@
+function countdown () {
+    for (let index = 0; index <= 2; index++) {
+        music.playTone(262, music.beat(BeatFraction.Quarter))
+        basic.showNumber(3 - index)
+    }
+    music.playTone(392, music.beat(BeatFraction.Whole))
+    basic.showString("GO!")
+}
+input.onButtonPressed(Button.A, function () {
+    countdown()
+})
+basic.forever(function () {
+	
+})
